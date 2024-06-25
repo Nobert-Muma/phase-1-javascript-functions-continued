89eec790cfa48f8152e50b90a19f18a3a6c13582
Nobert-Muma <mumanobert17@gmail.com>
2024-06-25 17:47:10 +0300

    Working with functions

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+function saturdayFun(firstArg) {
+    if (firstArg==="bathe my dog") {
+        return `This Saturday, I want to ${firstArg}!`;
+    } else {
+        return `This Saturday, I want to roller-skate!`;
+    }
+}
+saturdayFun("bathe my dog");
+
+
+ 
+function mondayWork(whatToDo = "go to the office") {
+    return `This Monday, I will ${whatToDo}.`;
+  }  
+   console.log(mondayWork()); 
+   console.log(mondayWork("work from home")); 
+  
+  
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("a dedicated programmer"));   
+  const criticalReviewFunction = wrapAdjective("(");
+  console.log(criticalReviewFunction("interesting", "&")); 
+  
